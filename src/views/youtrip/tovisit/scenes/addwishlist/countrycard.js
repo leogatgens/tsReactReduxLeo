78ca8007c474823cacd05706be9dada3da132ecb
Leonardo Meléndez <leogatgens@gmail.com>
2019-06-14 15:41:14 -0600

    ajustes

--- src/views/youtrip/tovisit/scenes/addwishlist/countrycard.js
+++ src/views/youtrip/tovisit/scenes/addwishlist/countrycard.js
@@ -1,69 +1,85 @@
-import { Card,Button,message,DatePicker } from 'antd';
-import React from 'react';
-import { GLOBALS} from '../../../globals/globals-variables';
-import moment from 'moment';
+import { Card, Button, message, DatePicker } from "antd";
+import React from "react";
+import { GLOBALS } from "../../../globals/globals-variables";
+import moment from "moment";
 
 let selectedDate = moment();
 
 class CountryCard extends React.Component {
-
-   onChange(date) { 
+  onChange(date) {
     selectedDate = date;
   }
-   AddItemToWishList = () =>{
-  
+  AddItemToWishList = () => {
     const newCountry = {
-      IdPais : this.props.valor.key,
-      DateTrip : selectedDate,
-      ClientId : this.props.props.data.data.data.auth.userProfile
-    };  
-    const serviceUrl = `${GLOBALS.rootAPI}/travelers/${newCountry.ClientId}/wishlists`;
-      var miInit = {               
-        headers : {
-          Authorization : `Bearer ${this.props.props.data.data.data.auth.getAccessToken()}`,
-          'Accept': 'application/json',  
-          'Content-Type': 'application/json'
-        },
-        method: 'post',
-        body: JSON.stringify(newCountry)       
-      }
+      IdPais: this.props.valor.key,
+      DateTrip: selectedDate,
+      ClientId: this.props.props.data.data.data.auth.userProfile
+    };
+    const serviceUrl = `${GLOBALS.rootAPI}/travelers/${
+      newCountry.ClientId
+    }/wishlists`;
+    var miInit = {
+      headers: {
+        Authorization: `Bearer ${this.props.props.data.data.data.auth.getAccessToken()}`,
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "post",
+      body: JSON.stringify(newCountry)
+    };
 
-       fetch(serviceUrl, miInit )         
-        .then(res => {    
-          if(res.ok)      
-          {
-           message.success('successfully added');   
-            
-           this.props.props.data.onAddItem();
+    fetch(serviceUrl, miInit)
+      .then(res => {
+        if (res.ok) {
+          message.success("successfully added");
 
-          }else{
-            message.error('Try again');
-          }
-          }
-        )
-       .catch(
-          (error) =>{ 
-          console.log(error);
-          message.error('Try again');
-          }
-        );           
-  }
-  render(){ 
-  
-    const valor = this.props.valor;     
-    return(
-        <div>
-        <Card title={valor.label} bordered={false} style={{ width: '100%', marginTop:10}}>
-          <p>Continente: {valor.label}  </p>
+          this.props.props.data.onAddItem();
+        } else {
+          message.error("Try again");
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        message.error("Try again");
+      });
+  };
+  render() {
+    const valor = this.props.valor;
+    return (
+      <div>
+        <Card
+          title={valor.label}
+          bordered={false}
+          style={{ width: "100%", marginTop: 10 }}
+        >
+          <p>Continente: {valor.label} </p>
           <p>Capital: {valor.label} </p>
-          <p>More information about   <a href={"https://www.google.com/search?q=" + valor.label }   target="_blank" rel="noopener noreferrer">{valor.label}</a></p>         
-          
-          <DatePicker defaultValue={moment().add(60,'days')} onChange={this.onChange} />
-          <Button style={{  marginLeft:10}} icon="plus"  onClick={this.AddItemToWishList}>Add to list</Button>
+          <p>
+            More information about{" "}
+            <a
+              href={"https://www.google.com/search?q=" + valor.label}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {valor.label}
+            </a>
+          </p>
+
+          <DatePicker
+            defaultValue={moment().add(60, "days")}
+            onChange={this.onChange}
+          />
+          <Button
+            style={{ marginLeft: 10 }}
+            icon="plus"
+            onClick={this.AddItemToWishList}
+          >
+            Add to list
+          </Button>
         </Card>
       </div>
     );
-}
+  }
 }
 
-export {CountryCard};
+export { CountryCard };
---
+++ src/views/youtrip/tovisit/scenes/tabsview.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Tabs } from "antd";
+import { WishList } from "./viewwishlist/wishlist";
+import { Wrapper } from "../../../../shared/estilos";
+import { TituloPrincipal } from "../../../../shared/estiloshtml";
+import { SelectCountry } from "./addwishlist/selectcountry";
+
+const TabPane = Tabs.TabPane;
+
+const TabsView = (props: any) => {
+  return (
+    <div>
+      {
+        <Tabs defaultActiveKey="1" onChange={props.callback}>
+          <TabPane tab="Wish list" key="1">
+            <WishList data={props} />
+          </TabPane>
+          <TabPane tab="Add to Wish List" key="2">
+            <Wrapper>
+              <TituloPrincipal>Países por visitar</TituloPrincipal>
+              <SelectCountry data={props} />
+            </Wrapper>
+          </TabPane>
+        </Tabs>
+      }
+    </div>
+  );
+};
+
+export default TabsView;
