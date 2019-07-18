0d57e85e55b7b68c8d099eb1907dd6919c2b6d6c
Leonardo Meléndez <leogatgens@gmail.com>
2019-07-18 16:12:49 -0600

    ajustes menu

--- src/globals/globals-variables.js
+++ src/globals/globals-variables.js
@@ -3,11 +3,11 @@ export const GLOBALS = {
   // rootWebSite : 'https://yourtripssite.z19.web.core.windows.net'
 
   //AWS
-  // rootAPI: 'https://trvewkinb5.execute-api.us-west-2.amazonaws.com/Prod/api',
-  // rootWebSite : 'https://youtripyouwish.s3.us-east-2.amazonaws.com/' ,
-  // rootImages : 'https://youtripyouwish.s3.us-east-2.amazonaws.com/img'
+  rootAPI: 'https://trvewkinb5.execute-api.us-west-2.amazonaws.com/Prod/api',
+  rootWebSite : 'https://youtripyouwish.s3.us-east-2.amazonaws.com/' ,
+  rootImages : 'https://youtripyouwish.s3.us-east-2.amazonaws.com/img'
 
-  rootAPI: "http://localhost:5208/api",
-  rootWebSite: "http://localhost:3000",
-  rootImages: "http://localhost:3000/img"
+  // rootAPI: "http://localhost:5208/api",
+  // rootWebSite: "http://localhost:3000",
+  // rootImages: "http://localhost:3000/img"
 };
--- src/shared/component/Header3.tsx
+++ src/shared/component/Header3.tsx
@@ -1,92 +1,98 @@
-import React from 'react';
-import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import FlightLand from '@material-ui/icons/FlightLand';
-import FlightTakeoff from '@material-ui/icons/FlightTakeoff';
-import VideogameAsset from '@material-ui/icons/VideogameAsset';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-interface Iprops  extends RouteComponentProps<any> {
-  
-}
+import React from "react";
+import {
+  fade,
+  makeStyles,
+  Theme,
+  createStyles
+} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import FlightLand from "@material-ui/icons/FlightLand";
+import FlightTakeoff from "@material-ui/icons/FlightTakeoff";
+import VideogameAsset from "@material-ui/icons/VideogameAsset";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+interface Iprops extends RouteComponentProps<any> {}
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     grow: {
-      flexGrow: 1,
+      flexGrow: 1
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+      marginRight: theme.spacing(2)
     },
     title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block"
+      }
     },
     search: {
-      position: 'relative',
+      position: "relative",
       borderRadius: theme.shape.borderRadius,
       backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
       },
       marginRight: theme.spacing(2),
       marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
         marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
+        width: "auto"
+      }
     },
     searchIcon: {
       width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
     },
     inputRoot: {
-      color: 'inherit',
+      color: "inherit"
     },
     inputInput: {
       padding: theme.spacing(1, 1, 1, 7),
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: 200,
-      },
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: 200
+      }
     },
     sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-      },
+      display: "none",
+      [theme.breakpoints.up("md")]: {
+        display: "flex"
+      }
     },
     sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
-    },
-  }),
+      display: "flex",
+      [theme.breakpoints.up("md")]: {
+        display: "none"
+      }
+    }
+  })
 );
 
- function PrimarySearchAppBar(props : Iprops) {
+function PrimarySearchAppBar(props: Iprops) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [
+    mobileMoreAnchorEl,
+    setMobileMoreAnchorEl
+  ] = React.useState<null | HTMLElement>(null);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -94,30 +100,29 @@ const useStyles = makeStyles((theme: Theme) =>
   function handleProfileMenuOpen(event: React.MouseEvent<HTMLElement>) {
     setAnchorEl(event.currentTarget);
   }
-  
 
   function handleGame(event: React.MouseEvent<HTMLElement>) {
     //setAnchorEl(event.currentTarget);
     const location = {
-      pathname: '/game'
-    }       
-   props.history.push(location)
+      pathname: "/game"
+    };
+    props.history.push(location);
   }
 
   function handleFlightLand(event: React.MouseEvent<HTMLElement>) {
     //setAnchorEl(event.currentTarget);
     const location = {
-      pathname: '/misviajes'
-    }       
-   props.history.push(location)
+      pathname: "/misviajes"
+    };
+    props.history.push(location);
   }
 
   function handleFlightTakeoff(event: React.MouseEvent<HTMLElement>) {
     //setAnchorEl(event.currentTarget);
     const location = {
-      pathname: '/porvisitar'
-    }       
-   props.history.push(location)
+      pathname: "/porvisitar"
+    };
+    props.history.push(location);
   }
   function handleMobileMenuClose() {
     setMobileMoreAnchorEl(null);
@@ -125,28 +130,27 @@ const useStyles = makeStyles((theme: Theme) =>
 
   function handleMenuClose() {
     console.log(props);
-    setAnchorEl(null);    
+    setAnchorEl(null);
     handleMobileMenuClose();
     const location = {
-      pathname: '/game'
-    }       
-   props.history.push(location)
-   //props.history.replace(location)
-    
+      pathname: "/game"
+    };
+    props.history.push(location);
+    //props.history.replace(location)
   }
 
   function handleMobileMenuOpen(event: React.MouseEvent<HTMLElement>) {
     setMobileMoreAnchorEl(event.currentTarget);
   }
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -155,32 +159,50 @@ const useStyles = makeStyles((theme: Theme) =>
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton aria-label="Show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
+        <IconButton
+          aria-label="Show 4 new mails"
+          color="inherit"
+          onClick={handleGame}
+        >
+          <Badge badgeContent={0} color="secondary">
+            <VideogameAsset />
+          </Badge>
+        </IconButton>
+        <p>Game</p>
+        <IconButton
+          aria-label="Show 4 new mails"
+          color="inherit"
+          onClick={handleFlightLand}
+        >
+          <Badge badgeContent={0} color="secondary">
             <FlightLand />
           </Badge>
         </IconButton>
-        <p>Messages</p>
+        <p>Mis viajes</p>
       </MenuItem>
       <MenuItem>
-        <IconButton aria-label="Show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
+        <IconButton
+          aria-label="Show 11 new notifications"
+          color="inherit"
+          onClick={handleFlightTakeoff}
+        >
+          <Badge badgeContent={0} color="secondary">
             <FlightTakeoff />
           </Badge>
         </IconButton>
-        <p>Notifications</p>
+        <p>Por visitar</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
@@ -209,7 +231,7 @@ const useStyles = makeStyles((theme: Theme) =>
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-          Tu diario de viaje 
+            Tu diario de viaje
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -219,24 +241,36 @@ const useStyles = makeStyles((theme: Theme) =>
               placeholder="Search…"
               classes={{
                 root: classes.inputRoot,
-                input: classes.inputInput,
+                input: classes.inputInput
               }}
-              inputProps={{ 'aria-label': 'Search' }}
+              inputProps={{ "aria-label": "Search" }}
             />
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-          <IconButton aria-label="Show 4 new mails" color="inherit" onClick={handleGame}>
+            <IconButton
+              aria-label="Show 4 new mails"
+              color="inherit"
+              onClick={handleGame}
+            >
               <Badge badgeContent={0} color="secondary">
                 <VideogameAsset />
               </Badge>
             </IconButton>
-            <IconButton aria-label="Show 4 new mails" color="inherit" onClick={handleFlightLand}>
+            <IconButton
+              aria-label="Show 4 new mails"
+              color="inherit"
+              onClick={handleFlightLand}
+            >
               <Badge badgeContent={4} color="secondary">
                 <FlightLand />
               </Badge>
             </IconButton>
-            <IconButton aria-label="Show 17 new notifications" color="inherit"   onClick={handleFlightTakeoff}>
+            <IconButton
+              aria-label="Show 17 new notifications"
+              color="inherit"
+              onClick={handleFlightTakeoff}
+            >
               <Badge badgeContent={17} color="secondary">
                 <FlightTakeoff />
               </Badge>
