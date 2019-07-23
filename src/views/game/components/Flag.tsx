3355be2e9245b882319dea0bbb9e9bee318dec02
Leonardo Meléndez <leogatgens@gmail.com>
2019-07-23 15:14:46 -0600

    cambiar control para mostar imagen

--- src/views/game/components/Flag.tsx
+++ src/views/game/components/Flag.tsx
@@ -1,23 +1,50 @@
-import React from 'react'; // we need this to make JSX compile
-import { Typography } from '@material-ui/core';
-import { IPais } from '../../../redux/InterfaceModels';
-import { GLOBALS } from '../../../globals/globals-variables';
+import React from "react"; // we need this to make JSX compile
+import {
+  Typography,
+  CardMedia,  
+  CardContent,
+  makeStyles,
+  createStyles
+} from "@material-ui/core";
+import { IPais } from "../../../redux/InterfaceModels";
+import { GLOBALS } from "../../../globals/globals-variables";
 
-
+import Card from "@material-ui/core/Card";
 interface IProps {
-  propiedades: IPais; 
-
+  propiedades: IPais;
 }
 
-export const CountryImage: React.SFC<IProps> = (props) => { 
-  console.log(props);
-  
-  return (     
-  <React.Fragment>    
-    <img src={props.propiedades.flagUrl ? props.propiedades.flagUrl.replace("##CDN##",GLOBALS.rootImages) : `${GLOBALS.rootImages}/notfound.png`} alt="logo"  />
-    <Typography variant="h6" align="center">
-      ¿País?
-    </Typography>
-    </React.Fragment>
-      );
-}
+const useStyles = makeStyles(
+  createStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 200,
+    },
+  }),
+);
+export const CountryImage: React.SFC<IProps> = props => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      
+        <CardMedia    className={classes.media}          
+          component="img"
+          alt="Contemplative Reptile"
+          image={
+            props.propiedades.flagUrl
+              ? props.propiedades.flagUrl.replace("##CDN##", GLOBALS.rootImages)
+              : `${GLOBALS.rootImages}/notfound.png`
+          }
+          title="Bandera Pais?"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Pais
+          </Typography>
+        </CardContent>
+      
+    </Card>
+  );
+};
